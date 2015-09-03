412c6e743f238966c1a1eb6226cd098bb752e3cf
David Page <david.page@psycle.com>
2015-09-03 12:05:05 +0100

    Fixes PHP notice since WordPress 4.3

--- includes/class-sensei-course-progress-widget.php
+++ includes/class-sensei-course-progress-widget.php
@@ -35,7 +35,7 @@ class Sensei_Course_Progress_Widget extends WP_Widget {
 		$control_ops = array( 'width' => 250, 'height' => 350, 'id_base' => $this->woo_widget_idbase );
 
 		/* Create the widget. */
-		$this->WP_Widget( $this->woo_widget_idbase, $this->woo_widget_title, $widget_ops, $control_ops );
+		parent::__construct( $this->woo_widget_idbase, $this->woo_widget_title, $widget_ops, $control_ops );
 	}
 
 	function widget( $args, $instance ) {
