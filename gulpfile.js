19a87bfa388456bda98fa88abf47783505edc203
Donna Peplinskie <donnapep@gmail.com>
2018-03-07 06:14:06 -0500

    Add build process to aid in deployment (#42)
    * Add package.json with dependencies needed for build
    
    * Add gulpfile.js that creates a /build folder for deployment
    
    Handles:
    - Minifying and renaming CSS and compiling resultant file to /build
    folder
    - Copying PHP and language files to /build folder
    - Adds file watcher task to recompile / move files automatically when
    changed
    
    * Don't commit /build folder
    
    * Don't commit built CSS file
    
    * Update CSS file name that gets loaded
    
    * Fix infinite loop when running gulp watch task
    
    * Fix php task
    
    * Add build-dev task to aid in local development

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+( () => {
+	'use strict';
+
+	const cleanCSS = require( 'gulp-clean-css' ),
+		del = require( 'del' ),
+		env = process.env.NODE_ENV || 'prod',
+		gulp = require( 'gulp' ),
+		rename = require( 'gulp-rename' ),
+		runSequence = require( 'run-sequence' ),
+		sass = require( 'gulp-sass' ),
+		sourcemaps = require( 'gulp-sourcemaps' );
+
+	const paths = {
+		css: [ 'assets/css/*.scss' ]
+	};
+
+	gulp.task( 'clean', function () {
+		return del( [ 'build' ] );
+	} );
+
+	gulp.task( 'css', function () {
+		return gulp.src( paths.css )
+			.pipe( sourcemaps.init() )
+			.pipe( sass( {
+				errLogToConsole: true,
+				outputStyle: 'expanded',
+			} ).on( 'error', sass.logError ) )
+			.pipe( cleanCSS() )
+			.pipe( sourcemaps.write() )
+			.pipe( rename( { suffix: '.min' } ) )
+			.pipe( gulp.dest( 'build/assets/css' ) )
+	} );
+
+	gulp.task( 'php', function() {
+		return gulp.src( [ '**/*.php', '!node_modules/**', '!build/**' ] )
+			.pipe( gulp.dest( 'build' ) );
+	} );
+
+	gulp.task( 'languages', function() {
+		return gulp.src( 'languages/*.*' )
+			.pipe( gulp.dest( 'build/languages' ) );
+	} );
+
+	gulp.task( 'watch', function() {
+		gulp.watch( paths.css, [ 'css' ]);
+		gulp.watch( [ '**/*.php', '!node_modules/**', '!build/**' ], [ 'php' ]);
+		gulp.watch( 'languages/*.*', [ 'languages' ]);
+	} );
+
+	gulp.task( 'build', function ( cb ) {
+		runSequence( 'clean', [ 'css', 'php', 'languages' ], cb );
+	} );
+
+	gulp.task( 'build-dev', function ( cb ) {
+		runSequence( 'clean', [ 'css', 'php', 'languages' ], [ 'watch' ], cb );
+	});
+
+	gulp.task( 'default', function( cb ) {
+		runSequence( 'build', cb );
+	} );
+} )();
