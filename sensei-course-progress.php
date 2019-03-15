f11af03901f1c6dfec441146d5e640bd151def1b
Alex Sanford <alex.sanford1@gmail.com>
2019-03-15 16:34:27 +0000

    Bump version number

--- sensei-course-progress.php
+++ sensei-course-progress.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Sensei Course Progress
- * Version: 1.0.8
+ * Version: 2.0.0-beta.1
  * Plugin URI: https://woocommerce.com/products/sensei-course-progress/
  * Description: Sensei extension that displays the learner's progress in the current course/module in a widget on lesson pages.
  * Author: Automattic
@@ -32,7 +32,7 @@ require_once( dirname( __FILE__ ) . '/includes/class-sensei-course-progress.php'
  * @return object Sensei_Course_Progress
  */
 function Sensei_Course_Progress() {
-	return Sensei_Course_Progress::instance( __FILE__, '1.0.8' );
+	return Sensei_Course_Progress::instance( __FILE__, '2.0.0-beta.1' );
 }
 
 Sensei_Course_Progress();
